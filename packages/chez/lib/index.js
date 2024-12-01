33e8a0114abf0787879def236acca28bd441913e
bencode <bencode@163.com>
2024-12-01 23:51:07 +0800

    fix load scheme.data and scheme.wasm

--- packages/chez/lib/index.js
+++ packages/chez/lib/index.js
@@ -1,12 +1,24 @@
 import SchemeModule from './scheme'
 
 export async function createEvaluator() {
-  const module = await SchemeModule()
+  const dataUrl = new URL('scheme.data', import.meta.url).href
+  const wasmUrl = new URL('scheme.wasm', import.meta.url).href;
+
+  const module = await SchemeModule({
+    locateFile: name => {
+      if (name === 'scheme.data') {
+        return dataUrl
+      }
+      if (name === 'scheme.wasm') {
+        return wasmUrl
+      }
+      return name
+    }
+  })
   const evaluator = new module.SchemeEvaluator()
 
   const evaluate = code => {
     return evaluator.evaluate(code)
   }
-
   return { evaluate }
 }
